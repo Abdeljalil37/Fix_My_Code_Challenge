a7da1a3054a57d085e08a9dd517224436a6b6eac
Abdeljalil37 <widadpop@gmail.com>
2024-01-29 18:22:49 +0100

    try fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
+""" Class Sqaure """
 
-class square():
-    width = 0
-    height = 0
+class Square:
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a Square object.
 
-
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        :param width: The width of the square.
+        :param height: The height of the square.
+        """
+        self.width = width
+        self.height = height
 
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Calculate the area of the square."""
         return self.width * self.width
 
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """Calculate the perimeter of the square."""
         return (self.width * 2) + (self.height * 2)
 
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """Return a string representation of the square."""
+        return f"{self.width}/{self.height}"
 
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
